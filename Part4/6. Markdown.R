3976922071c04c06860c66a7c9d833b8c79cb407
psm9619 <soominp17@gmail.com>
2019-06-15 16:47:11 +0900

    R data analysis

--- Part4/6. Markdown.R
+++
@@ -1,4 +0,0 @@
-# Markdown
-# using markdown, you can creat a report/paper that includes all words, R codes, and R grphics
-
-# In RStudio, go to File-> New-> R Markdown (it already has everything you need to start including '''{r})
